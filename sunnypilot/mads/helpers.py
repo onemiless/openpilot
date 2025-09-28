3500de7ecb06cdce0c9727c4ccb989abbb23897c
dzid26 <dzidmail@gmail.com>
2025-09-28 01:31:47 +0100

    experimental - force-enable mads settings

--- sunnypilot/mads/helpers.py
+++ sunnypilot/mads/helpers.py
@@ -9,7 +9,6 @@ from openpilot.common.params import Params
 from opendbc.car import structs
 from opendbc.safety import ALTERNATIVE_EXPERIENCE
 from opendbc.sunnypilot.car.hyundai.values import HyundaiFlagsSP, HyundaiSafetyFlagsSP
-from opendbc.sunnypilot.car.tesla.values import TeslaFlagsSP
 
 
 MADS_NO_ACC_MAIN_BUTTON = ("rivian", "tesla")
@@ -24,8 +23,6 @@ class MadsSteeringModeOnBrake:
 def get_mads_limited_brands(CP: structs.CarParams, CP_SP: structs.CarParamsSP) -> bool:
   if CP.brand == 'rivian':
     return True
-  if CP.brand == 'tesla':
-    return not CP_SP.flags & TeslaFlagsSP.HAS_VEHICLE_BUS
 
   return False
 
