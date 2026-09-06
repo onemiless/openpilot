ad6a98d122aefb18276152f042e3505814ab7c8c
onemiles <690299233@qq.com>
2026-09-06 15:06:43 +0800

    models: register chestnut error state

--- openpilot/sunnypilot/modeld_v2/tests/test_egpu_loading.py
+++ openpilot/sunnypilot/modeld_v2/tests/test_egpu_loading.py
@@ -1,6 +1,7 @@
 import threading
 import unittest
 
+from openpilot.common.params import Params
 from openpilot.sunnypilot.modeld_v2.egpu_loader import (
   C3XL_AM_POWER_LIMIT_W, C3XL_AMD_USB_POLL_US, C3XL_MODEL_LOAD_TIMEOUT, C3XL_TINYGRAD_CACHE_HOME,
   EgpuModelLoadError, configure_default_device, load_with_timeout,
@@ -8,6 +9,9 @@ from openpilot.sunnypilot.modeld_v2.egpu_loader import (
 
 
 class TestEgpuLoading(unittest.TestCase):
+  def test_chestnut_model_error_param_is_registered(self):
+    self.assertIn(b"ChestnutModelError", Params().all_keys())
+
   def test_timeout_covers_measured_c3xl_model_loads(self):
     measured_max_seconds = 75.58
     self.assertEqual(C3XL_MODEL_LOAD_TIMEOUT, 120)
