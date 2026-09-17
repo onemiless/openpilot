905c1145567fd42a1577ed6c89af132c62493a9a
onemiles <690299233@qq.com>
2026-09-17 22:30:14 +0800

    modeld: propagate modelDataV2SP validity

--- openpilot/sunnypilot/modeld_v2/modeld.py
+++ openpilot/sunnypilot/modeld_v2/modeld.py
@@ -658,6 +658,7 @@ def main(demo=False):
       )
       modelv2_send.modelV2.meta.laneChangeState = DH.lane_change_state
       modelv2_send.modelV2.meta.laneChangeDirection = DH.lane_change_direction
+      mdv2sp_send.valid = modelv2_send.valid
       mdv2sp_send.modelDataV2SP.laneTurnDirection = DH.lane_turn_direction
       drivingdata_send.drivingModelData.meta.laneChangeState = DH.lane_change_state
       drivingdata_send.drivingModelData.meta.laneChangeDirection = DH.lane_change_direction
