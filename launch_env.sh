b2b6c3e86eef1ca9948ac67d8d5227947b2fec40
onemiles <690299233@qq.com>
2026-09-17 23:10:47 +0800

    agnos: select manifests by hardware profile

--- launch_env.sh
+++ launch_env.sh
@@ -15,8 +15,27 @@ export VECLIB_MAXIMUM_THREADS=1
 # headroom for this until ui is moved to the CPU.
 export QCOM_PRIORITY=12
 
-if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="19.6"
+SP_PROFILE_VALUE="${SUNNYPILOT_HARDWARE_PROFILE:-}"
+SP_PROFILE_FILE="${SUNNYPILOT_HARDWARE_PROFILE_FILE:-/data/hardware_profile}"
+SP_MODEL_FILE="${SUNNYPILOT_HARDWARE_MODEL_FILE:-/sys/firmware/devicetree/base/model}"
+if [ -z "$SP_PROFILE_VALUE" ] && [ -f "$SP_PROFILE_FILE" ]; then
+  SP_PROFILE_VALUE="$(tr -d '\000\r\n ' < "$SP_PROFILE_FILE")"
 fi
+if [ -z "$SP_PROFILE_VALUE" ]; then
+  SP_MODEL_VALUE=""
+  if [ -f "$SP_MODEL_FILE" ]; then
+    SP_MODEL_VALUE="$(tr -d '\000\r\n ' < "$SP_MODEL_FILE")"
+  fi
+  [ "$SP_MODEL_VALUE" = "comma tici" ] && SP_PROFILE_VALUE="c3xl" || SP_PROFILE_VALUE="standard"
+fi
+
+if [ "$SP_PROFILE_VALUE" = "c3xl" ]; then
+  [ -z "$AGNOS_VERSION" ] && export AGNOS_VERSION="19.6"
+  [ -z "$AGNOS_MANIFEST_FILE" ] && export AGNOS_MANIFEST_FILE="openpilot/system/hardware/comma/agnos-c3xl.json"
+else
+  [ -z "$AGNOS_VERSION" ] && export AGNOS_VERSION="19.7"
+  [ -z "$AGNOS_MANIFEST_FILE" ] && export AGNOS_MANIFEST_FILE="openpilot/system/hardware/comma/agnos.json"
+fi
+unset SP_PROFILE_VALUE SP_PROFILE_FILE SP_MODEL_FILE SP_MODEL_VALUE
 
 export STAGING_ROOT="/data/safe_staging"
